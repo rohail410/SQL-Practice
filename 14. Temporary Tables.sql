b14be1529580906dbc820c772f1a5e793041de52
rohail410 <rohailtabassumit@gmail.com>
2025-01-16 22:59:04 +0500

    Temporary Tables Practice

---
+++ 14. Temporary Tables.sql
@@ -0,0 +1,46 @@
+-- Temporary Table
+
+CREATE TEMPORARY TABLE temp_table 
+(first_name varchar(50),
+last_name varchar(50),
+favorite_movie varchar(100)
+);
+
+SELECT *
+FROM temp_table;
+
+
+INSERT INTO temp_table
+VALUES("Rohail", "Tabassum", "Harry Potter");
+
+SELECT *
+FROM temp_table;
+
+
+SELECT *
+FROM employee_salary;
+
+CREATE TEMPORARY TABLE salary_over_50k 
+(
+Select *
+FROM employee_salary
+WHERE salary >= 50000
+)
+;
+
+SELECT *
+FROM salary_over_50k;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
